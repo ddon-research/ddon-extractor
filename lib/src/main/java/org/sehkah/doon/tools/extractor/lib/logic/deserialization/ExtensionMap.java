87cb9590ed6ad4c07e2e21f1bcd91983c5f49d62
Sehkah <sehkah.vhemrada@gmail.com>
2023-05-05 16:45:15 +0200

    Document supported file extensions.

--- lib/src/main/java/org/sehkah/doon/tools/extractor/lib/logic/deserialization/ExtensionMap.java
+++ lib/src/main/java/org/sehkah/doon/tools/extractor/lib/logic/deserialization/ExtensionMap.java
@@ -5,6 +5,8 @@ import org.sehkah.doon.tools.extractor.lib.logic.deserialization.game_common.Ene
 import org.sehkah.doon.tools.extractor.lib.logic.deserialization.gui_cmn.StageMapDeserializer;
 import org.sehkah.doon.tools.extractor.lib.logic.deserialization.gui_cmn.WarpLocationDeserializer;
 
+import java.util.Arrays;
+
 public enum ExtensionMap {
     EMG("emg", null, 1, EnemyGroupDeserializer.class),
     WAL("wal", null, 353, WarpLocationDeserializer.class),
@@ -19,25 +21,25 @@ public enum ExtensionMap {
     JLT2("jlt2", null, 1, JobLevelUpTableDeserializer.class),
     UNSUPPORTED("", null, 0, null);
 
-    public final String resourceFileExtension;
+    static {
+        System.out.println((Arrays.toString(ExtensionMap.values())));
+    }
+
+    public final String fileExtension;
     public final String magic;
     public final long version;
     public final Class<? extends Deserializer> deserializer;
 
-    ExtensionMap(String resourceFileExtension, String magic, long version, Class<? extends Deserializer> deserializer) {
-        this.resourceFileExtension = resourceFileExtension;
+    ExtensionMap(String fileExtension, String magic, long version, Class<? extends Deserializer> deserializer) {
+        this.fileExtension = fileExtension;
         this.magic = magic;
         this.version = version;
         this.deserializer = deserializer;
     }
 
-    /**
-     * This method matches on the file extension at the end of the path and ensures the arc file path is contained as well.
-     * This avoids issues when the resource file name is different but the extraction uses the same deserializer.
-     */
     public static ExtensionMap findByFileExtension(String filePath) {
         for (ExtensionMap value : ExtensionMap.values()) {
-            if (filePath.endsWith(value.resourceFileExtension)) {
+            if (filePath.endsWith(value.fileExtension)) {
                 return value;
             }
         }
@@ -46,11 +48,8 @@ public enum ExtensionMap {
 
     @Override
     public String toString() {
-        return "ExtensionMap{" +
-                "resourceFileExtension='" + resourceFileExtension + '\'' +
-                ", magic='" + magic + '\'' +
-                ", version=" + version +
-                ", deserializer=" + deserializer +
-                '}';
+        return "{\n\"fileExtension\": \"" + fileExtension + "\",\n"
+                + (magic != null ? "\"magic\": \"" + magic + "\",\n" : "")
+                + "\"version\": " + version + "\n}";
     }
 }
