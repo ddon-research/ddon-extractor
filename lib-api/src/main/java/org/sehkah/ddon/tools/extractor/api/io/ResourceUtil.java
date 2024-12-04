4dc97645f16494190afbf26a0d3596360179340a
Sehkah <sehkah.vhemrada@gmail.com>
2024-12-04 22:05:14 +0100

    Improve resource recognition and add known door IDs.

--- lib-api/src/main/java/org/sehkah/ddon/tools/extractor/api/io/ResourceUtil.java
+++ lib-api/src/main/java/org/sehkah/ddon/tools/extractor/api/io/ResourceUtil.java
@@ -2,8 +2,12 @@ package org.sehkah.ddon.tools.extractor.api.io;
 
 import org.sehkah.ddon.tools.extractor.api.error.TechnicalException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class ResourceUtil {
     private ResourceUtil() {
@@ -11,8 +15,17 @@ public class ResourceUtil {
     }
 
     public static byte[] loadResourceFile(String resourceFilePath) {
-        try (InputStream in = ResourceUtil.class.getResourceAsStream(resourceFilePath)) {
+        try (InputStream in = ResourceUtil.class.getClassLoader().getResourceAsStream(resourceFilePath)) {
             return in.readAllBytes();
+        } catch (IOException e) {
+            throw new TechnicalException(e);
+        }
+    }
+
+    public static List<String> loadResourceFileLines(String resourceFilePath) {
+        try (InputStream in = ResourceUtil.class.getClassLoader().getResourceAsStream(resourceFilePath)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            return reader.lines().toList();
         } catch (IOException e) {
             throw new TechnicalException(e);
         }
